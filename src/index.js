ac6b8153b9be8a30f02d45e7de7a37a679ab0c6a
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-07 22:27:32 -0700

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -53,5 +53,5 @@ function searchCityTemp(city) {
   let apiKey = `b0b9a67412cc5694fd13908f533da803`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-  axios.get(apiUrl).then(searchCityValue);
+  axios.get(apiUrl).then(displayWeather);
 }
