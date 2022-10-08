fd56fb265e67ec4e1d9a20f086c487ffa4496ff9
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-07 18:13:19 -0700

    Created index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+let now = new Date();
+console.log(now.getDate());
+
+let todayDate = document.querySelector("#todayDate");
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+let year = now.getFullYear();
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+todayDate.innerHTML = `${day}, ${month} ${date}, ${year} ${hours}:${minutes}`;
+function displayWeather(response) {
+  let humidity = Math.round(response.data.main.humidity);
+  let humidElement = document.querySelector("#today-humidity");
+  humidElement.innerHTML = `${humidity}`;
+  let temperature = Math.round(response.data.main.temp);
+  let tempElement = document.querySelector("#today-temp");
+  tempElement.innerHTML = `${temperature}`;
+}
+
+function searchCityValue(event) {
+  event.preventDefault();
+  let input = document.querySelector("#search-form-input");
+  let output = document.querySelector("#chosen-city");
+  output.innerHTML = input.value;
+  searchCityTemp(input.value);
+}
+let form = document.querySelector("form");
+form.addEventListener("submit", searchCityValue);
+
+function searchCityTemp(city) {
+  let units = "imperial";
+  let apiKey = `b0b9a67412cc5694fd13908f533da803`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
