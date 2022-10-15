b031744d494321dbc939e02f0c5df561b11a970f
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-15 00:16:26 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,10 +42,11 @@ function displayWeather(response) {
   let windElement = document.querySelector("#wind-speed");
   windElement.innerHTML = `${wind}km/hr`;
   let iconElement = document.querySelector("#weather-icon");
-  iconElement.setAttribute(
+  iconElement.innerHTML.setAttribute(
     "src",
-    `images${response.data.weather[0].description}.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function searchCityValue(event) {
   event.preventDefault();
