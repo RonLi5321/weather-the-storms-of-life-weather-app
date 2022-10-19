14aefb88407b948c9d2b4e50eb19f35f116450df
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-18 18:05:32 -0700

    Integrating SheCodes weather API

--- src/index.js
+++ src/index.js
@@ -94,7 +94,8 @@ let fahrenheitTemp = document.querySelector("#fahrenheit-temp");
 fahrenheitTemp.addEventListener("click", showFahrenheitTemperature);
 function searchCityTemp(city) {
   let units = "imperial";
-  let apiKey = `b0b9a67412cc5694fd13908f533da803`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayWeather);
+  let query = "city";
+  let apiKey = `8eddda0f7cctb6ac729734523f46ob41`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
 }
+axios.get(apiUrl).then(displayWeather);
