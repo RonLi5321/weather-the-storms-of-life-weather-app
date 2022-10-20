66e6860b2f3049166755283668720fcb158e29ec
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-19 23:17:20 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -77,6 +77,13 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  let apiKey = "b0b9a67412cc5694fd13908f533da803";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayWeather(response) {
   let output = document.querySelector("#chosen-city");
