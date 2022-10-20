2cb2f01f4b741b64a8b467ee671eaf7b6ab6e3b3
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-19 23:41:50 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -109,7 +109,7 @@ function changeIcon(iconCode) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
+  let apiKey = "b0b9a67412cc5694fd13908f533da803";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
