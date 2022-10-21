319a5e932fc4905604bd9378cc52ac08fb5b1711
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-21 07:20:23 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,6 +18,22 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
   return `${day}, ${month} ${date}, ${year} ${hours}:${minutes}`;
 }
 function displayForecast() {
