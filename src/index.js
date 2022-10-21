524b72121fd6e3fe53c84e51cb2b741259ac19b6
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-20 18:50:14 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,3 +20,21 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = `
+  <div class="row">
+    <div class="col-2">
+      <div class="weather-forecast-date">Thu</div>
+      <img
+        src="http://openweathermap.org/img/wn/50d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">85°F </span>
+        <span class="weather-forecast-temperature-min">65°F</span>
+      </div>
+    </div>
+  </div>`;
+}
