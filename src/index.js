0e07522350485161a508d4e4d3cfb42707c49316
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-20 18:48:29 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,119 +1,22 @@
-let now = new Date();
-console.log(now.getDate());
-
-let todayDate = document.querySelector("#todayDate");
-let date = now.getDate();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let seconds = now.getSeconds();
-let year = now.getFullYear();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-todayDate.innerHTML = `${day}, ${month} ${date}, ${year} ${hours}:${minutes}`;
-
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = `
-  <div class="row">
-    <div class="col-2">
-      <div class="weather-forecast-date">Thu</div>
-      <img
-        src="http://openweathermap.org/img/wn/50d@2x.png"
-        alt=""
-        width="42"
-      />
-      <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-max">85°F </span>
-        <span class="weather-forecast-temperature-min">65°F</span>
-      </div>
-    </div>
-  </div>`;
-}
-function displayWeather(response) {
-  let output = document.querySelector("#chosen-city");
-  output.innerHTML = response.data.name;
-  let humidity = Math.round(response.data.main.humidity);
-  let humidElement = document.querySelector("#today-humidity");
-  humidElement.innerHTML = `${humidity}%`;
-  fahrenheitTemperature = response.data.main.temp;
-  let temperature = Math.round(fahrenheitTemperature);
-  let tempElement = document.querySelector("#today-temp");
-  tempElement.innerHTML = `${temperature}`;
-  let descriptionElement = document.querySelector("#weather-description");
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  let wind = Math.round(response.data.wind.speed);
-  let windElement = document.querySelector("#wind-speed");
-  windElement.innerHTML = `${wind}km/hr`;
-  let iconElement = document.querySelector("#weather-icon");
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-  let dateElement = document.querySelector("#todayDate");
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-}
-function searchCityValue(event) {
-  event.preventDefault();
-  let input = document.querySelector("#search-form-input");
-  let output = document.querySelector("#chosen-city");
-  output.innerHTML = input.value;
-  searchCityTemp(input.value);
-}
-let form = document.querySelector("form");
-form.addEventListener("button", searchCityValue);
-form.addEventListener("submit", searchCityValue);
-function showCelsiusTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#today-temp");
-  let cTemp = (fahrenheitTemperature - 32) / 1.8;
-  temperatureElement.innerHTML = Math.round(cTemp);
-}
-function showFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#today-temp");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-let fahrenheitTemperature = null;
-
-let celsiusTemp = document.querySelector("#celsius-temp");
-celsiusTemp.addEventListener("click", showCelsiusTemperature);
-let fahrenheitTemp = document.querySelector("#fahrenheit-temp");
-fahrenheitTemp.addEventListener("click", showFahrenheitTemperature);
-function searchCityTemp(city) {
-  let units = "imperial";
-  let apiKey = `b0b9a67412cc5694fd13908f533da803`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayWeather);
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
