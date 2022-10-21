ecef00b7817279496fa671c47ba7ad2c39384354
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-20 22:55:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function displayForecast() {
 
   forecastHTML =
     forecastHTML +
-    `<div class="col-15">
+    `<div class="col">
     <div class="card" style="width: 12rem;">
      <div class="card-header text-secondary" class="weather-forecast-date">Thu</div>
      <div class="card-body">
@@ -47,7 +47,7 @@ function displayForecast() {
   `;
   forecastHTML =
     forecastHTML +
-    `<div class="col-15">
+    `<div class="col">
     <div class="card" style="width: 12rem;">
       <div class="card-header text-secondary" class="weather-forecast-date">Thu</div>
       <div class="card-body">
@@ -66,7 +66,7 @@ function displayForecast() {
   `;
   forecastHTML =
     forecastHTML +
-    `<div class="col-15">
+    `<div class="col">
     <div class="card" style="width: 12rem;">
       <div class="card-header text-secondary" class="weather-forecast-date">Thu</div>
       <div class="card-body">
@@ -85,7 +85,7 @@ function displayForecast() {
   `;
   forecastHTML =
     forecastHTML +
-    `<div class="col-15">
+    `<div class="col">
     <div class="card" style="width: 12rem;">
       <div class="card-header text-secondary" class="weather-forecast-date">Thu</div>
       <div class="card-body">
@@ -104,7 +104,7 @@ function displayForecast() {
   `;
   forecastHTML =
     forecastHTML +
-    `<div class="col-15">
+    `<div class="col">
     <div class="card" style="width: 12rem;">
       <div class="card-header text-secondary" class="weather-forecast-date">Thu</div>
       <div class="card-body">
