5150ea519dc8b2964c3e43bb6d1a12174f2ceef1
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-20 22:51:29 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function displayForecast() {
 
   forecastHTML =
     forecastHTML +
-    `<div class="col-6">
+    `<div class="col-15">
     <div class="card" style="width: 12rem;">
      <div class="card-header text-secondary" class="weather-forecast-date">Thu</div>
      <div class="card-body">
@@ -47,7 +47,7 @@ function displayForecast() {
   `;
   forecastHTML =
     forecastHTML +
-    `<div class="col-6">
+    `<div class="col-15">
     <div class="card" style="width: 12rem;">
       <div class="card-header text-secondary" class="weather-forecast-date">Thu</div>
       <div class="card-body">
