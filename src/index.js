01e3f1bdbf64cd2f2b40ec3a95b69338efeec63d
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-20 23:49:00 -0700

    Formatted .js and .css

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
