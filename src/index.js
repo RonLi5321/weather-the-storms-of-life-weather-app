4fb375829d3d9b41a67120009ff6f1a0fdcbc249
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-20 18:25:41 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -43,7 +43,21 @@ todayDate.innerHTML = `${day}, ${month} ${date}, ${year} ${hours}:${minutes}`;
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = "Forecast";
+  forecastElement.innerHTML = "";
+  <div class="row">
+    <div class="col-2">
+      <div class="weather-forecast-date">Thu</div>
+      <img
+        src="http://openweathermap.org/img/wn/50d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">85°F </span>
+        <span class="weather-forecast-temperature-min">65°F</span>
+      </div>
+    </div>
+  </div>;
 }
 function displayWeather(response) {
   let output = document.querySelector("#chosen-city");
