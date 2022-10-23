3e00eff96b90ab010216196241e3d96baaed51a1
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-23 15:02:24 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,6 @@ function displayWeather(response) {
   let temperature = Math.round(fahrenheitTemperature);
   let tempElement = document.querySelector("#today-temp");
   tempElement.innerHTML = `${temperature}`;
-  getForecast(response.data.coord);
   let descriptionElement = document.querySelector("#weather-description");
   descriptionElement.innerHTML = response.data.weather[0].description;
   let wind = Math.round(response.data.wind.speed);
