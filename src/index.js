53bfd1aed7c4edffa725baa11869a02e13837678
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-23 09:27:30 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -108,7 +108,6 @@ function displayWeather(response) {
 }
 
 function searchCityTemp(city) {
-  let units = "imperial";
   let key = `8eddda0f7cctb6ac729734523f46ob41`;
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=imperial`;
   axios.get(apiUrl).then(displayWeather);
