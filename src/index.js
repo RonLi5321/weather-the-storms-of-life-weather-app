c31fb6e1a91ceb468fe84567d287cab2d981af3b
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-23 17:42:33 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -43,8 +43,8 @@ todayDate.innerHTML = `${day}, ${month} ${date}, ${year} ${hours}:${minutes}`;
 function formatDay(date) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-
-  return day;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
 }
 function displayForecast(response) {
   let forecast = response.data.daily;
