539377c33b0fe50700f97cf27054e724a74c1308
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-23 17:36:48 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,27 +40,28 @@ let months = [
 ];
 let month = months[now.getMonth()];
 todayDate.innerHTML = `${day}, ${month} ${date}, ${year} ${hours}:${minutes}`;
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
-  days.forEach(function (day) {
+
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `<div class="col-sm-2">
     <div class="card" style="width: 12rem;">
-     <div class="card-header text-secondary" class="weather-forecast-date">${day}</div>
+     <div class="card-header text-secondary" class="weather-forecast-date">${forecastDay.dt}</div>
      <div class="card-body">
       
       <img
-        src="http://openweathermap.org/img/wn/50d@2x.png"
+        src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}50d@2x.png"
         alt=""
         width="42"
       />
       <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-max">85°F </span>
-        <span class="weather-forecast-temperature-min">65°F</span>
+        <span class="weather-forecast-temperature-max">${forecastDay.temp.max} °</span>
+        <span class="weather-forecast-temperature-min">${forecastDay.temp.min}°</span>
       </div>
       </div>
   </div>
