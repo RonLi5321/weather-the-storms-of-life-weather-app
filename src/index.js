f6abb13135184a8a769c0667ed365bb9b01b6faa
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-23 17:37:54 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ let months = [
 ];
 let month = months[now.getMonth()];
 todayDate.innerHTML = `${day}, ${month} ${date}, ${year} ${hours}:${minutes}`;
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
