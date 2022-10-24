4efb406b86bfcf7b8194bf889220e14da35eab5e
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-23 19:52:11 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6 && index > 0) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `<div class="col-sm-2">
