5e49f6ab986f10bd6d7a8b20c28701333f8ddab3
RonLi5321 <ronna.del.rosario@gmail.com>
2022-10-23 20:44:13 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -64,8 +64,9 @@ function displayForecast(response) {
      <div class="card-body">
       
       <img
-        src="http://openweathermap.org/img/wn/${
-          forecastDay.weather[0].icon
+         src=""http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+           forecastDay.condition[0].icon
+         }.png"
         }@2x.png"
         alt=""
         width="42"
@@ -92,8 +93,8 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = `e947cb2640f1db92e6a19005bc43b435`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  let key = `e947cb2640f1db92e6a19005bc43b435`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${key}}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 function displayWeather(response) {
